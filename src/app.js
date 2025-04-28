dbb43f6965572cdd3636ad61fcf28cfe5f5aa7fb
Phùng Văn Tiến Đạt <tiendat060709@gmail.com>
2025-04-28 15:47:28 +0700

    create project

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
+const app = express();
+
+// init middleware
+
+// +init morgan
+app.use(morgan('dev'));
+// app.use(morgan('combined'));
+// app.use(morgan('common'));
+// app.use(morgan('short'));
+// app.use(morgan('tiny'));
+
+// +init helmet
+app.use(helmet());
+
+// +init compression
+app.use(compression());
+
+//init database
+
+// init routes
+app.get('/', (req, res, next) => {
+    const strCompression = 'Hello Compression'
+    return res.status(200).json({
+        message: 'Hello World!',
+        metadata: strCompression.repeat(1000),
+    });
+});
+
+//inter handling errors
+
+
+module.exports = app;
